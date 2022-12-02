bad2f70cf188e74d6c363cb73d415656bcef0893
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-02 23:00:03 +0200

    python scoping 1

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-responses = {}
 
-polling_active = True
-
-while polling_active:
-    name = input("What is your name? ")
-    response = input('What career do you want to have? ')
-    responses[name] = response
-    repeat = input("Do you want to be asked again? (yes/no) ")
-    if(repeat == 'no'):
-        polling_active = False
-
-
-print(responses)
+def my_function():
+    test = 1  # this is defined in the local scope of the function
+    print('my_function:', test)
+test = 0  # this is defined in the global scope
+my_function()
+print('global:', test)
