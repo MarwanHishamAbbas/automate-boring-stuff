6d9ae7baa7dca99dcdef4a204b4e4b1631c07f94
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-03 02:16:56 +0200

    collecting dict as params

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# ** means collecting data in dictionary
+def connect(**options):
+    conn_params = {
+        'host': options.get('host', '127.0.0.1'),
+        'port': options.get('port', '3000'),
+        'user': options.get('user', 'guest'),
+    }
+    print(conn_params)
+    # db.connect()
 
-def my_function():
-    test = 1  # this is defined in the local scope of the function
-    print('my_function:', test)
-test = 0  # this is defined in the global scope
-my_function()
-print('global:', test)
+
+connect()
+connect( user='Marwan Hisham', port=8080)
