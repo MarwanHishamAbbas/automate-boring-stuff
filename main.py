20453468a5796dc0cb238ca95c14a56218838621
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-05 00:41:54 +0200

    @decorator

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-# ** means collecting data in dictionary
-def connect(**options):
-    conn_params = {
-        'host': options.get('host', '127.0.0.1'),
-        'port': options.get('port', '3000'),
-        'user': options.get('user', 'guest'),
-    }
-    print(conn_params)
-    # db.connect()
+# Decorators are a very powerful and useful tool in Python since it allows programmers to modify the behaviour of a function or class. Decorators allow us to wrap another function in order to extend the behaviour of the wrapped function, without permanently modifying it. 
 
+# decorators/time.measure.deco1.py
 
-connect()
-connect( user='Marwan Hisham', port=8080)
+def hello_decorator(func):
+    def inner1(*args, **kwargs):
+         
+        print("before Execution")
+         
+        # getting the returned value
+        returned_value = func(*args, **kwargs)
+        print("after Execution")
+         
+        # returning the value to the original frame
+        return returned_value
+         
+    return inner1
+ 
+ 
+# adding decorator to the function
+@hello_decorator
+def sum_two_numbers(a, b):
+    print("Inside the function")
+    return a + b
+ 
+a, b = 1, 2
+ 
+# getting the value through return of the function
+print("Sum =", sum_two_numbers(a, b))
