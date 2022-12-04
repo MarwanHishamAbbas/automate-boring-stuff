166aa08b3a02af7f94045081418b9918a3437f1b
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-05 01:09:54 +0200

    @decorators

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-responses = {}
+# Decorators are a very powerful and useful tool in Python since it allows programmers to modify the behaviour of a function or class. Decorators allow us to wrap another function in order to extend the behaviour of the wrapped function, without permanently modifying it. 
 
-polling_active = True
+# decorators/time.measure.deco1.py
 
-while polling_active:
-    name = input("What is your name? ")
-    response = input('What career do you want to have? ')
-    responses[name] = response
-    repeat = input("Do you want to be asked again? (yes/no) ")
-    if(repeat == 'no'):
-        polling_active = False
-
-
-print(responses)
+def hello_decorator(func):
+    def inner1(*args, **kwargs):
+         
+        print("before Execution")
+         
+        # getting the returned value
+        returned_value = func(*args, **kwargs)
+        print("after Execution")
+         
+        # returning the value to the original frame
+        return returned_value
+         
+    return inner1
+ 
+ 
+# adding decorator to the function
+@hello_decorator
+def sum_two_numbers(a, b):
+    print("Inside the function")
+    return a + b
+ 
+a, b = 1, 2
+ 
+# getting the value through return of the function
+print("Sum =", sum_two_numbers(a, b))
