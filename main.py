82761c30129c29492a1fae7de29d266e83a60976
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-05 15:12:44 +0200

    basic oop syntax

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-# Decorators are a very powerful and useful tool in Python since it allows programmers to modify the behaviour of a function or class. Decorators allow us to wrap another function in order to extend the behaviour of the wrapped function, without permanently modifying it. 
+# Object-oriented programming (OOP) is a programming paradigm based on the concept of "objects", which are data structures that contain data, in the form of attributes, and code, in the form of functions known as methods. A distinguishing feature of objects is that an object's method can access and often modify the data attributes of the object with which they are associated (objects have a notion of "self"). In OO programming, computer programs are designed by making them out of objects that interact with one another.    
 
-# decorators/time.measure.deco1.py
+class Rectangle(): 
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+        self.side_b = side_b
+        
+    def area(self):
+        return self.side_b * self.side_b
 
-def hello_decorator(func):
-    def inner1(*args, **kwargs):
-         
-        print("before Execution")
-         
-        # getting the returned value
-        returned_value = func(*args, **kwargs)
-        print("after Execution")
-         
-        # returning the value to the original frame
-        return returned_value
-         
-    return inner1
- 
- 
-# adding decorator to the function
-@hello_decorator
-def sum_two_numbers(a, b):
-    print("Inside the function")
-    return a + b
- 
-a, b = 1, 2
- 
-# getting the value through return of the function
-print("Sum =", sum_two_numbers(a, b))
+
+shape = Rectangle(10, 20)
+print(shape.area())
