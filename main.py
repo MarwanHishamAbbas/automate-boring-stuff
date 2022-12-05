f95e0cb806210da235d5f1ffd2a889912a3a7007
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-05 17:19:58 +0200

    super().__init__

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-# Object-oriented programming (OOP) is a programming paradigm based on the concept of "objects", which are data structures that contain data, in the form of attributes, and code, in the form of functions known as methods. A distinguishing feature of objects is that an object's method can access and often modify the data attributes of the object with which they are associated (objects have a notion of "self"). In OO programming, computer programs are designed by making them out of objects that interact with one another.    
-
-class Rectangle(): 
-    def __init__(self, side_a, side_b):
-        self.side_a = side_a
-        self.side_b = side_b
+# oop/super.implicit.py
+class Book:
+    def __init__(self, title, publisher, pages):
+        self.title = title
+        self.publisher = publisher
+        self.pages = pages
         
-    def area(self):
-        return self.side_b * self.side_b
-
-
-shape = Rectangle(10, 20)
-print(shape.area())
+class Ebook(Book):
+    def __init__(self, title, publisher, pages, format_):
+        super().__init__(title, publisher, pages)
+        # Another way to do the same thing is:
+        # super(Ebook, self).__init__(title, publisher, pages)
+        self.format_ = format_
+ebook = Ebook(
+    'Learn Python Programming', 'Packt Publishing', 500, 'PDF')
+print(ebook.title) # Learn Python Programming
+print(ebook.publisher) # Packt Publishing
+print(ebook.pages) # 500
+print(ebook.format_) # PDF
