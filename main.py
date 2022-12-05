7657e65fb708499447b20f3288a24a2bba187345
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-05 19:20:40 +0200

    encapsulation"

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-class Shape:
-    geometric_type = 'Generic Shape'
-    def area(self):  # This acts as placeholder for the interface
-        raise NotImplementedError
-    def get_geometric_type(self):
-        return self.geometric_type
-class Plotter:
-    def plot(self, ratio, topleft):
-        # Imagine some nice plotting logic here...
-        print('Plotting at {}, ratio {}.'.format(
-            topleft, ratio))
-class Polygon(Shape, Plotter):  # base class for polygons
-    pass
+class Student: 
+    no_of_students = 0
+    def __init__(self, name, age, courses):
+        self.__name = name
+        self.__age = age
+        self.__courses = courses
+        Student.no_of_students += 1
+    
+    def display(self):
+        print(f"My name is {self.__name}, I'm {self.__age} years old and I'm studing {self.__courses[1]} ")
 
-geometric_type = 'Polygon'
-class RegularPolygon(Polygon):  # Is-A Polygon
-    geometric_type = 'Regular Polygon'
-    def __init__(self, side):
-        self.side = side
-class RegularHexagon(RegularPolygon):  # Is-A RegularPolygon
-    geometric_type = 'RegularHexagon'
-    def area(self):
-        return 1.5 * (3 ** .5 * self.side ** 2)
-class Square(RegularPolygon):  # Is-A RegularPolygon
-    geometric_type = 'Square'
-    def area(self):
-        return self.side * self.side
-        
 
-# Python will go all the way up to the base class
-hexagon = RegularHexagon(10) # looking for the nearest init and pass the 10 to it
-print(hexagon.area())  # 259.8076211353316
-print(hexagon.get_geometric_type())  # RegularHexagon
-hexagon.plot(0.8, (75, 77))  # Plotting at (75, 77), ratio 0.8.
-square = Square(12)
-print(square.area())  # 144
-print(square.get_geometric_type())  # Square
-square.plot(0.93, (74, 75))  # Plotting at (74, 75), ratio 0.93.
+
+
+student_1 = Student('Marwan Hisham', 23, ['Math', 'Science'])
+
+student_1.display()
