1ea3e7f4702cff26e1c3c15ed5f7950ab967b37c
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-05 18:56:20 +0200

    inheritance + composition

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-# oop/super.implicit.py
-class Book:
-    def __init__(self, title, publisher, pages):
-        self.title = title
-        self.publisher = publisher
-        self.pages = pages
+class Shape:
+    geometric_type = 'Generic Shape'
+    def area(self):  # This acts as placeholder for the interface
+        raise NotImplementedError
+    def get_geometric_type(self):
+        return self.geometric_type
+class Plotter:
+    def plot(self, ratio, topleft):
+        # Imagine some nice plotting logic here...
+        print('Plotting at {}, ratio {}.'.format(
+            topleft, ratio))
+class Polygon(Shape, Plotter):  # base class for polygons
+    pass
+
+geometric_type = 'Polygon'
+class RegularPolygon(Polygon):  # Is-A Polygon
+    geometric_type = 'Regular Polygon'
+    def __init__(self, side):
+        self.side = side
+class RegularHexagon(RegularPolygon):  # Is-A RegularPolygon
+    geometric_type = 'RegularHexagon'
+    def area(self):
+        return 1.5 * (3 ** .5 * self.side ** 2)
+class Square(RegularPolygon):  # Is-A RegularPolygon
+    geometric_type = 'Square'
+    def area(self):
+        return self.side * self.side
         
-class Ebook(Book):
-    def __init__(self, title, publisher, pages, format_):
-        super().__init__(title, publisher, pages)
-        # Another way to do the same thing is:
-        # super(Ebook, self).__init__(title, publisher, pages)
-        self.format_ = format_
-ebook = Ebook(
-    'Learn Python Programming', 'Packt Publishing', 500, 'PDF')
-print(ebook.title) # Learn Python Programming
-print(ebook.publisher) # Packt Publishing
-print(ebook.pages) # 500
-print(ebook.format_) # PDF
+
+# Python will go all the way up to the base class
+hexagon = RegularHexagon(10) # looking for the nearest init and pass the 10 to it
+print(hexagon.area())  # 259.8076211353316
+print(hexagon.get_geometric_type())  # RegularHexagon
+hexagon.plot(0.8, (75, 77))  # Plotting at (75, 77), ratio 0.8.
+square = Square(12)
+print(square.area())  # 144
+print(square.get_geometric_type())  # Square
+square.plot(0.93, (74, 75))  # Plotting at (74, 75), ratio 0.93.
