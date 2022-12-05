b2fc62b0bed9800cc6163a24b3b408284cbec622
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-06 01:33:18 +0200

    understanding @classmethod

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Student: 
     no_of_students = 0
     def __init__(self, name, age, courses):
@@ -9,9 +11,8 @@ class Student:
     def display(self):
         print(f"My name is {self.__name}, I'm {self.__age} years old and I'm studing {self.__courses[1]} ")
 
-
-
-
-student_1 = Student('Marwan Hisham', 23, ['Math', 'Science'])
-
-student_1.display()
+@classmethod
+def initFromDateTime(cls, name, birthYear):
+    # This is like alternative __init__ to the original one
+    return cls(name, date.today().year - birthYear)
+    # __init__(name, age=date.today().year - birthYear)
