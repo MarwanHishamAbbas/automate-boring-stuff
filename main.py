63b472427d45219d452ab25c52694c1c5b0abfb6
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-06 14:07:24 +0200

    json basics

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-responses = {}
+# putting code within the body of a try block adds a mechanism around that code that allows us to detect errors (which are called exceptions) and decide what to do if they happen
 
-polling_active = True
+# try:
+#     fh = open('text.txt')
+#     for line in fh:
+#         print(line.strip())
+# finally:
+#     fh.close()
 
-while polling_active:
-    name = input("What is your name? ")
-    response = input('What career do you want to have? ')
-    responses[name] = response
-    repeat = input("Do you want to be asked again? (yes/no) ")
-    if(repeat == 'no'):
-        polling_active = False
+# Open files using context managers
+# with open('text.txt') as fh:
+#     for line in fh:
+#         print(line.strip())
 
+# files/existence.py
+# import os
+# filename = 'text.txt'
+# path = os.path.dirname(os.path.abspath(filename))
+# print(os.path.isfile(filename))  # True
+# print(os.path.isdir(path))  # True
+# print(path)  # /Users/fab/srv/lpp/ch7/files
 
-print(responses)
+# import json
+
+# info = {
+#     'full_name': 'Sherlock Holmes',
+#     'address': {
+#         'street': '221B Baker St',
+#         'zip': 'NW1 6XE',
+#         'city': 'London',
+#         'country': 'UK',
+# }}
+# # json.dumps py => json
+# # json.loads json => py
+# print(json.dumps(info, indent=2, sort_keys=True))
